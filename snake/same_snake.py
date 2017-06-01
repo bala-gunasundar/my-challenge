5284e8813f295349ca805a398be5b04dcadb73d1
Balamanikandan Gunasundar <bgunasun@visteon.com>
2017-06-01 19:47:44 +0530

    Complete problem

--- snake/same_snake.py
+++ snake/same_snake.py
@@ -1,10 +1,12 @@
+# Problem : https://www.codechef.com/SNCKQL17/problems/SAMESNAK
+
 #!/usr/bin/python
 
 # Get direction of sname (Only axis parallel)
 def get_direction(head, tail):
     x = 0
-    y = 1
 
+    y = 1
     # 2,1   2, -2
     if head[x] > tail[x] and head[y] == tail[y]:
         direction = 'left'
@@ -19,6 +21,7 @@ def get_direction(head, tail):
 
     return direction
 
+# Collect vertices between head and tail
 def get_vertices(head, tail):
     vertices = []
     d = get_direction(head, tail)
@@ -48,6 +51,7 @@ def get_vertices(head, tail):
     return vertices
 
 
+# find the next node in direction (right/left/up/down)
 def neighbour(vertex, direction):
     # eg: vertex = (2, 1)
     x = 0
@@ -64,23 +68,23 @@ def neighbour(vertex, direction):
         raise IOError, 'Direction not axis parallel'
 
 opposite = {'right' : 'left', 'left' : 'right', 'up' : 'down', 'down': 'up'}
+
+# Get edges for all the nodes between head and tail.
+# Get edges in both forward and reverse direction
 def get_edges(head, tail):
-    #head = s[0]
-    #tail = s[1]
     x = 0
     y = 1
     edge = {}
 
     direction = get_direction(head, tail)
-    print 'direction =', direction
 
-    # right
+    # head to tail
     for vertex in get_vertices(head, tail):
         if vertex == tail:
             continue
         edge[vertex] = [neighbour(vertex, direction)]
 
-    #left
+    # tail to head
     for vertex in get_vertices(tail, head):
         # No edge for head and tail
         if vertex == head:
@@ -120,12 +124,14 @@ def union(e1, e2):
 
     return dest
 
+# helper
 def display_edges(e):
     print '[','*' * 10
     for key in e.keys():
         print '\t%s = %s' %(key, e[key])
     print '*' * 10, ']'
 
+# if a vertex has > 2 edges, return false
 def check_edges(e):
     for key in e.keys():
         if len(e[key]) > 2:
@@ -133,9 +139,40 @@ def check_edges(e):
 
     return True
 
-def connected(head, tail, edges):
+visited = {}
 
-    return True
+def print_visited(edges):
+    for key in edges.keys():
+        print 'visited[%s] = %s' %(key, visited[key])
+
+# Search for all vertices of the snake and mark the visited
+def search(edges, vertex):
+
+    visited[vertex] = True
+
+    for node in edges[vertex]:
+        if visited[node] == False:
+                search(edges, node)
+
+# Return true if all the vertex of the snake is connected
+# and all vertex have edges <= 2
+def is_connected(edges, head):
+
+    for key in edges.keys():
+        visited[key] = False
+
+    search(edges, head)
+
+    connected = True
+    for key in edges.keys():
+        if not visited[key]:
+            connected = False
+            break
+
+    if connected:
+        return True
+    else:
+        return False
 
 def same_snake(s1, s2):
     head1 = s1[0]
@@ -144,21 +181,21 @@ def same_snake(s1, s2):
     head2 = s2[0]
     tail2 = s2[1]
 
-    print head1, tail1
+    # Collected vertices and edges for snake sighting1
     vertices1 = get_vertices(head1, tail1)
-    print 'vertices =', vertices1
     edges1 = get_edges(head1, tail1)
     #display_edges(edges1)
-    print head2, tail2
+
+    # Collected vertices and edges for snake sighting2
     vertices2 = get_vertices(head2, tail2)
-    print 'vertices =', vertices2
     edges2 = get_edges(head2, tail2)
     #display_edges(edges2)
-    print 'Union ='
+
     edges = union(edges1, edges2)
-    display_edges(edges)
-    if check_edges(edges):
-        print 'Yes'
+    #display_edges(edges)
+
+    if check_edges(edges) and is_connected(edges, head1):
+        print 'yes'
     else:
         print 'no'
 
@@ -176,20 +213,17 @@ def main():
 
     s1 = [(2, 1), (8, 1)]
     s2 = [(11, 1), (7, 1)]
-
     s3 = s1
     s4 = [(11, 1), (9, 1)]
-
     s5 = s1
     s6 = [(3, 1), (3, -2)]
-
     s7 = s1
     s8 = [(2, 1), (2, -2)]
 
     same_snake(s1, s2)
     same_snake(s3, s4)
     same_snake(s5, s6)
-    #same_snake(s7, s8)
+    same_snake(s7, s8)
 
 
 if __name__ == '__main__':

634a24abe34fd1325bb99f781d80ebda5bb0a16a
Balamanikandan Gunasundar <bgunasun@visteon.com>
2017-06-01 15:53:49 +0530

    Add functions to get edges and vertices between two vertices

--- snake/same_snake.py
+++ snake/same_snake.py
@@ -15,51 +15,108 @@ def get_direction(head, tail):
     elif head[y] > tail[y] and head[x] == tail[x]:
         direction = 'down'
     else:
-        direction = 'error'
+        raise IOError, 'Vertex not axis parallel'
 
     return direction
 
-# Get edges of all vertices
-def get_edges(s):
-    head = s[0]
-    tail = s[1]
+def get_vertices(head, tail):
+    vertices = []
+    d = get_direction(head, tail)
+
     x = 0
     y = 1
-    edge = []
-
-    direction = get_direction(head, tail)
-    if direction == 'right':
+    vertices = []
+    if d == 'right':
         for i in range(head[x], tail[x] + 1):
-            edge.append([i, head[y]])
+            vertices.append((i, head[y]))
 
-    elif direction == 'left':
+    elif d == 'left':
         for i  in range(head[x], tail[x] - 1, -1):
-            edge.append([i, head[y]])
+            vertices.append((i, head[y]))
 
-    elif direction == 'up':
+    elif d == 'up':
         for i in range(head[y], tail[y] + 1):
-            edge.append([head[x], i])
+            vertices.append((head[x], i))
 
-    elif direction == 'down':
+    elif d == 'down':
         for i in range(head[y], tail[y] - 1, -1):
-            edge.append([head[x], i])
+            vertices.append((head[x], i))
 
     else:
-        edge = -1
+        raise IOError, 'Direction not axis parallel'
+
+    return vertices
+
+
+def neighbour(vertex, direction):
+    # eg: vertex = (2, 1)
+    x = 0
+    y = 1
+    if direction == 'right':
+        return (vertex[x] + 1, vertex[y])
+    elif direction == 'left':
+        return (vertex[x] - 1, vertex[y])
+    elif direction == 'up':
+        return (vertex[x], vertex[y] + 1)
+    elif direction == 'down':
+        return (vertex[x], vertex[y] - 1)
+    else:
+        raise IOError, 'Direction not axis parallel'
+
+def get_edges(head, tail):
+    #head = s[0]
+    #tail = s[1]
+    x = 0
+    y = 1
+    edge = {}
+
+    direction = get_direction(head, tail)
+    print direction
+    for vertex in get_vertices(head, tail):
+        edge[vertex] = neighbour(vertex, direction)
+
+    # Last one doesn't have an edge
+    del edge[tail]
 
     return edge
 
+def next_vertex(vertex, direction):
+    x = 0
+    y = 1
+
+    if direction == 'right':
+        return (vertex[x] + 1, vertex[y])
+    elif direction == 'left':
+        return (vertex[x] - 1, vertex[y])
+    elif direction == 'up':
+        return (vertex[x], vertex[y] + 1)
+    elif direction == 'down':
+        return (vertex[x], vertex[y] - 1)
+    else:
+        raise IOError, 'Invalid direction: %s' %direction
+
+
 # Return (s1 U s2)
 def union(s1, s2):
-    s = s1 + s2
+    s = set(s1) | set(s2)
     return s
 
 def same_snake(s1, s2):
-    e1 = get_edges(s1)
-    print 'e1 = ', e1
-    e2 = get_edges(s2)
-    print 'e2 = ', e2
-    print union(e1, e2)
+    head1 = s1[0]
+    tail1 = s1[1]
+
+    head2 = s2[0]
+    tail2 = s2[1]
+
+    print head1, tail1
+    print get_vertices(head1, tail1)
+    print get_edges(head1, tail1)
+
+    print '*' * 10
+    print head2, tail2
+    print get_vertices(head2, tail2)
+    print get_edges(head2, tail2)
+
 
 def main():
     # no = int(raw_input())
@@ -73,9 +130,13 @@ def main():
     #     print s1
     #     print s2
 
-    s1 = [[2, 1], [8, 1]]
-    s2 = [[3, 1], [3, -2]]
+    s1 = [(2, 1), (8, 1)]
+    s2 = [(11, 1), (7, 1)]
 
-    same_snake(s1, s2)
+    s3 = [(2,1), (8,1)]
+    s4 = [(3,1), (3,8)]
+    same_snake(s3, s4)
+
+
 if __name__ == '__main__':
     main()
